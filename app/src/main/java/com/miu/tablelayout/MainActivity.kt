e2ab8ba78f005fb0c746fccfed199aeefe82d42d
youyou19 <juleswilfrid9@gmail.com>
2020-09-07 18:20:14 -0400

    this

--- app/src/main/java/com/miu/tablelayout/MainActivity.kt
+++ app/src/main/java/com/miu/tablelayout/MainActivity.kt
@@ -1,50 +1,68 @@
 package com.miu.tablelayout
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
-import android.widget.TableLayout
+import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.TableRow
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-
+import android.graphics.Color
+import androidx.core.view.marginTop
+import androidx.core.view.setPadding
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        addBtn.setOnClickListener{
-            // Create a new table row.
-            val tableRow = TableRow(getApplicationContext())
-            // Set new table row layout parameters.
-            val layoutParams = TableRow.LayoutParams(TableLayout.LayoutParams.FILL_PARENT,TableRow.LayoutParams.WRAP_CONTENT)
-            tableRow.setLayoutParams(layoutParams)
- //add values into row by calling addView()
-        var txtversion:TextView =TextView(this)
-            var txtname:TextView =TextView(this)
-         //   txtversion.height=1;
-       //     txtname.height=1;
-      //     txtname.textSize=15F
-         //   txtversion.textSize=15F
-            txtversion.setLayoutParams(TableRow.LayoutParams(TableLayout.LayoutParams.FILL_PARENT,
-                TableLayout.LayoutParams.WRAP_CONTENT))
 
-            txtname.setLayoutParams(TableRow.LayoutParams(TableLayout.LayoutParams.FILL_PARENT,
-                TableLayout.LayoutParams.WRAP_CONTENT))
+        add.setOnClickListener{
+            if(version.text.toString().isNullOrEmpty()||codeName.text.toString().isNullOrEmpty()) {
+                Toast.makeText(this, "Please Enter Android version and code name", Toast.LENGTH_LONG).show()
 
-           txtversion.setText(addAndroidVersionText.getText())
-            txtversion.setGravity(Gravity.CENTER)
-       //     txtversion.setPadding(15,15,15,15)
+            }
+            else{
+                val tableRow = TableRow(getApplicationContext())
 
-            txtname.setText(addAndroidCodeText.getText())
-            txtname.setGravity(Gravity.CENTER)
-        //    txtname.setPadding(100,15,900,15)
+// Set new table row layout parameters.
+                val layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT)
+                layoutParams.setMargins(5, 5, 5, 5)
+                tableRow.setLayoutParams(layoutParams)
 
-             tableRow.addView(txtversion, 0)
-            tableRow.addView(txtname, 1)
-// Finally add the created row row into layout
-            layoutId.addView(tableRow) // id from Layout_file
+
+
+//create textview
+                val Tv1 = TextView(this)
+
+                Tv1.text = version.text.toString()
+                Tv1.layoutParams = layoutParams
+                Tv1.textSize = 20F
+                Tv1.setBackgroundColor(Color.parseColor("#D81B60"))
+
+
+                val Tv2 = TextView(this)
+                Tv2.text = codeName.text.toString()
+                Tv2.layoutParams = layoutParams
+                Tv2.textSize = 20F
+
+                Tv2.setBackgroundColor(Color.parseColor("#D81B60"))
+
+
+// add values into row by calling addView()
+                tableRow.addView(Tv1, 0)
+                tableRow.addView(Tv2, 1)
+
+// Finally add the created row into layout
+                tab.addView(tableRow) // id from Layout_file
+
+                // clear the  version and codeName Textboxes
+                version.text=null
+                codeName.text=null
+
+            }
+
         }
+
     }
+
 }
